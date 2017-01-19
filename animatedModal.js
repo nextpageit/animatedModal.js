f7c96f96effe9d5b8409581964a2892504d0bc0e
nitin-dhiman <nitin@nextpage.co.in>
2017-01-19 18:29:08 +0530

    Close Button works after AJAX

--- animatedModal.js
+++ animatedModal.js
@@ -1,132 +1,133 @@
-   /*=========================================
-     * animatedModal.js: Version 1.0
-     * author: João Pereira
-     * website: http://www.joaopereira.pt
-     * email: joaopereirawd@gmail.com
-     * Licensed MIT
-     =========================================*/
+/*=========================================
+ * animatedModal.js: Version 1.0
+ * author: João Pereira
+ * website: http://www.joaopereira.pt
+ * email: joaopereirawd@gmail.com
+ * Licensed MIT
+ =========================================*/
 
-    (function ($) {
-        $.fn.animatedModal = function (options) {
-            var modal = $(this);
+(function ($) {
+    $.fn.animatedModal = function (options) {
+        var modal = $(this);
 
-            //Defaults
-            var settings = $.extend({
-                modalTarget: 'animatedModal',
-                position: 'fixed',
-                width: '100%',
-                height: '100%',
-                top: '0px',
-                left: '0px',
-                zIndexIn: '9999',
-                zIndexOut: '-9999',
-                color: '#39BEB9',
-                opacityIn: '1',
-                opacityOut: '0',
-                animatedIn: 'zoomIn',
-                animatedOut: 'zoomOut',
-                animationDuration: '.6s',
-                overflow: 'auto',
-                // Callbacks
-                beforeOpen: function () {
-                },
-                afterOpen: function () {
-                },
-                beforeClose: function () {
-                },
-                afterClose: function () {
-                }
-
-
-            }, options);
-
-            var closeBt = $('.close-' + settings.modalTarget);
-
-            //console.log(closeBt)
-
-            var href = '#animatedModal',
-                    id = $('body').find('#' + settings.modalTarget),
-                    idConc = '#' + id.attr('id');
-            //console.log(idConc);
-            // Default Classes
-            id.addClass('animated');
-            id.addClass(settings.modalTarget + '-off');
-
-            //Init styles
-            var initStyles = {
-                'position': settings.position,
-                'width': settings.width,
-                'height': settings.height,
-                'top': settings.top,
-                'left': settings.left,
-                'background-color': settings.color,
-                'overflow-y': settings.overflow,
-                'z-index': settings.zIndexOut,
-                'opacity': settings.opacityOut,
-                '-webkit-animation-duration': settings.animationDuration,
-                '-moz-animation-duration': settings.animationDuration,
-                '-ms-animation-duration': settings.animationDuration,
-                'animation-duration': settings.animationDuration
-            };
-            //Apply stles
-            id.css(initStyles);
-
-            modal.click(function (event) {
-                event.preventDefault();
-                var ctrl = $(event.target);
-                console.log(ctrl);
-                $('body, html').css({'overflow': 'hidden'});
-                if (href == idConc) {
-                    if (id.hasClass(settings.modalTarget + '-off')) {
-                        id.removeClass(settings.animatedOut);
-                        id.removeClass(settings.modalTarget + '-off');
-                        id.addClass(settings.modalTarget + '-on');
-                    }
-
-                    if (id.hasClass(settings.modalTarget + '-on')) {
-                        var url = ctrl.data("ajax");
-                        if (typeof(url) != "undefined" && url.length > 0)
-                            $.get(url, null, function (data) {
-                                $(".animated-modal-content").html(data);
-                            })
-                        settings.beforeOpen();
-                        id.css({'opacity': settings.opacityIn, 'z-index': settings.zIndexIn});
-                        id.addClass(settings.animatedIn);
-                        id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterOpen);
-                    }
-                    ;
-                }
-            });
+        //Defaults
+        var settings = $.extend({
+            modalTarget: 'animatedModal',
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            top: '0px',
+            left: '0px',
+            zIndexIn: '9999',
+            zIndexOut: '-9999',
+            color: '#39BEB9',
+            opacityIn: '1',
+            opacityOut: '0',
+            animatedIn: 'zoomIn',
+            animatedOut: 'zoomOut',
+            animationDuration: '.6s',
+            overflow: 'auto',
+            // Callbacks
+            beforeOpen: function () {
+            },
+            afterOpen: function () {
+            },
+            beforeClose: function () {
+            },
+            afterClose: function () {
+            }
 
 
-            closeBt.click(function (event) {
-                event.preventDefault();
-                $('body, html').css({'overflow': 'auto'});
+        }, options);
 
-                settings.beforeClose(); //beforeClose
-                if (id.hasClass(settings.modalTarget + '-on')) {
-                    id.removeClass(settings.modalTarget + '-on');
-                    id.addClass(settings.modalTarget + '-off');
-                }
+        var closeBtSel = '.close-' + settings.modalTarget;
+        var closeBt = $(closeBtSel);
 
+        //console.log(closeBt)
+
+        var href = '#animatedModal',
+                id = $('body').find('#' + settings.modalTarget),
+                idConc = '#' + id.attr('id');
+        //console.log(idConc);
+        // Default Classes
+        id.addClass('animated');
+        id.addClass(settings.modalTarget + '-off');
+
+        //Init styles
+        var initStyles = {
+            'position': settings.position,
+            'width': settings.width,
+            'height': settings.height,
+            'top': settings.top,
+            'left': settings.left,
+            'background-color': settings.color,
+            'overflow-y': settings.overflow,
+            'z-index': settings.zIndexOut,
+            'opacity': settings.opacityOut,
+            '-webkit-animation-duration': settings.animationDuration,
+            '-moz-animation-duration': settings.animationDuration,
+            '-ms-animation-duration': settings.animationDuration,
+            'animation-duration': settings.animationDuration
+        };
+        //Apply stles
+        id.css(initStyles);
+
+        modal.click(function (event) {
+            event.preventDefault();
+            var ctrl = $(event.target);
+            console.log(ctrl);
+            $('body, html').css({'overflow': 'hidden'});
+            if (href == idConc) {
                 if (id.hasClass(settings.modalTarget + '-off')) {
-                    id.removeClass(settings.animatedIn);
-                    id.addClass(settings.animatedOut);
-                    id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterClose);
+                    id.removeClass(settings.animatedOut);
+                    id.removeClass(settings.modalTarget + '-off');
+                    id.addClass(settings.modalTarget + '-on');
+                }
+
+                if (id.hasClass(settings.modalTarget + '-on')) {
+                    var url = ctrl.data("ajax");
+                    if (typeof(url) != "undefined" && url.length > 0)
+                        $.get(url, null, function (data) {
+                            $(".animated-modal-content").html(data);
+                        })
+                    settings.beforeOpen();
+                    id.css({'opacity': settings.opacityIn, 'z-index': settings.zIndexIn});
+                    id.addClass(settings.animatedIn);
+                    id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterOpen);
                 }
                 ;
+            }
+        });
 
-            });
 
-            function afterClose() {
-                id.css({'z-index': settings.zIndexOut});
-                settings.afterClose(); //afterClose
+        $(body).on("click", closeBtSel, function (event) {
+            event.preventDefault();
+            $('body, html').css({'overflow': 'auto'});
+
+            settings.beforeClose(); //beforeClose
+            if (id.hasClass(settings.modalTarget + '-on')) {
+                id.removeClass(settings.modalTarget + '-on');
+                id.addClass(settings.modalTarget + '-off');
             }
 
-            function afterOpen() {
-                settings.afterOpen(); //afterOpen
+            if (id.hasClass(settings.modalTarget + '-off')) {
+                id.removeClass(settings.animatedIn);
+                id.addClass(settings.animatedOut);
+                id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterClose);
             }
+            ;
 
-        }; // End animatedModal.js
+        });
 
-    }(jQuery));
+        function afterClose() {
+            id.css({'z-index': settings.zIndexOut});
+            settings.afterClose(); //afterClose
+        }
+
+        function afterOpen() {
+            settings.afterOpen(); //afterOpen
+        }
+
+    }; // End animatedModal.js
+
+}(jQuery));
