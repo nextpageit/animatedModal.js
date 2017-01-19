44d9f94416bec9fa9efab76d5741bf649b0fa29b
nitin-dhiman <nitin@nextpage.co.in>
2017-01-19 12:45:06 +0530

    Update animatedModal.js

--- animatedModal.js
+++ animatedModal.js
@@ -87,7 +87,7 @@
                         var url = ctrl.data("ajax");
                         if (typeof(url) != "undefined" && url.length > 0)
                             $.get(url, null, function (data) {
-                                $(".modal-content").html(data.base);
+                                $(".modal-content").html(data);
                             })
                         settings.beforeOpen();
                         id.css({'opacity': settings.opacityIn, 'z-index': settings.zIndexIn});
